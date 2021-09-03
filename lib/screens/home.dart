7412928acb6496ccc32d1323395b7a7ebe88bd02
ariefsaferman <arief.saferman@ui.ac.id>
2021-09-03 16:05:28 +0700

    First Launch BMI apps

---
+++ lib/screens/home.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/cupertino.dart';
+import 'package:tutorial/constants/app_constants.dart';
+import 'package:tutorial/widget/right_bar.dart';
+import 'package:tutorial/widget/left_bar.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController _heightController = TextEditingController(); 
+  TextEditingController _weightController = TextEditingController(); 
+  double _bmiResult = 0; 
+  String _textResult = ""; 
+  @override
+  Widget build(BuildContext context) {
+    final ButtonStyle style = ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold));
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "BMI Calculator",
+          style: TextStyle(color: accentHexColor, fontWeight: FontWeight.w300
+        ),
+      ),
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      centerTitle: true,
+      ),
+      backgroundColor: mainHexColor,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 20,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  width: 130,
+                  child: TextField(
+                    controller: _heightController,
+                    style: TextStyle(
+                      fontSize: 42,
+                      fontWeight: FontWeight.w300,
+                      color: accentHexColor
+                    ),
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Height",
+                      hintStyle: TextStyle(
+                        fontSize: 42, 
+                        fontWeight: FontWeight.w300, 
+                        color: Colors.white.withOpacity(.8)
+                      )
+                    ),
+                  ),
+                ), 
+
+                Container(
+                  width: 130,
+                  child: TextField(
+                    controller: _weightController,
+                    style: TextStyle(
+                      fontSize: 42,
+                      fontWeight: FontWeight.w300,
+                      color: accentHexColor
+                    ),
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Weight",
+                      hintStyle: TextStyle(
+                        fontSize: 42, 
+                        fontWeight: FontWeight.w300, 
+                        color: Colors.white.withOpacity(.8)
+                      )
+                    ),
+                  ),
+                ), 
+
+              ],
+            ),
+            SizedBox(height: 30,),
+            GestureDetector(
+              child: ElevatedButton(
+                style: style, 
+                onPressed: (){
+                  double _h = double.parse(_heightController.text); 
+                  double _w = double.parse(_weightController.text);
+                  setState(() {
+                  _bmiResult = _w / (_h * _h); 
+                  if(_bmiResult > 25){
+                    _textResult = "You're over weight"; 
+                  } else if(_bmiResult >= 18.5 && _bmiResult <=  25){
+                    _textResult = "You have normal weight"; 
+                  } else{
+                    _textResult = "You're under weight";
+                  }
+                });
+                }, 
+                child: Text("Calculate")),
+            ),
+
+            SizedBox(height: 50,),
+            Container(
+              child: Text(_bmiResult.toStringAsFixed(2), style: TextStyle(fontSize: 22,  color: accentHexColor),),
+            ),
+            SizedBox(height: 30,),
+            Visibility(
+              visible: _textResult.isNotEmpty,
+              child: Container(
+                child: Text(_textResult, style: TextStyle(fontSize: 22, fontWeight: FontWeight.w400, color: accentHexColor),),
+              )
+              
+            ),
+            SizedBox(height: 10,),
+            const LeftBar(barWidth: 40),
+            SizedBox(height: 20,),
+            const LeftBar(barWidth: 70),
+            SizedBox(height: 20,),
+            const LeftBar(barWidth: 40),
+
+            SizedBox(height: 20,),
+            const RightBar(barWidth: 70),
+            SizedBox(height: 50,),
+            const RightBar(barWidth: 70),
+
+
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
